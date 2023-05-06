479362f1d91e0cac02141f5cde6c60396bf918d8
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-06 21:08:25 +0530

    Update dao.sol

--- dao.sol
+++ dao.sol
@@ -25,3 +25,27 @@ uint public nextProposalId;
 uint public voteTime;
 uint public quorum;
 address public manager;
+
+constructor (uint _participationTimeEnd , uint _voteTime , uint _quorum){
+     participationTimeEnd = block.timestamp+ _participationTimeEnd; 
+     voteTime = _voteTime;
+     quorum = _quorum;
+     manager=msg.sender;
+ }
+    modifier onlyInvestor(){
+      require(isInvestor[msg.sender]==true,"You are not an investor");
+      _;
+}
+    modifier onlyManager(){
+     require(manager==msg.sender,"You are not a manager");
+      _;
+}
+    function contribution() public payable{
+    require(participationTimeEnd>=block.timestamp,"Contribution Time Ended");//5 PM , //16342343342 + 2*3600 > 16342340000
+    require(msg.value>0,"Send more than 0 ether");
+    isInvestor[msg.sender]=true;
+    numOfshares[msg.sender]=numOfshares[msg.sender]+msg.value;
+    totalShares+=msg.value; //totaltShares=totalShares+msg.value
+    availableFunds+=msg.value;
+    investorsList.push(msg.sender);
+}
